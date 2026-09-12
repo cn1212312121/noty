2a09bfaa4f629deae2cd979e6bfaccc278a534ef
cn1993x <157273371+cn1212312121@users.noreply.github.com>
2026-09-12 15:31:04 +0700

    fix

--- check_site.py
+++ check_site.py
@@ -8,7 +8,8 @@ URLS = [
     "https://demox.store",
     "https://mobile.demox.store",
     "https://demoy.store",
-    "https://mobile.demoy.store"
+    "https://mobile.demoy.store",
+    "http://okcasino11.com"
 ]
 BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
